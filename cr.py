e7f71a328a7336a65dc5bf8e484d09426366d974
Manuel Castiblanco <71740021+mcastiblanco1251@users.noreply.github.com>
2022-11-03 12:42:46 -0500

    Add files via upload

--- cr.py
+++ cr.py
@@ -22,12 +22,14 @@ hide_menu_style = """
         <style>
         #MainMenu {visibility: hidden;}
         footer {visibility: hidden;}
-
+        .checkbox-text {font-size:114px;}
         </style>
         """
 st.markdown(hide_menu_style, unsafe_allow_html=True)
 
-
+imagel = Image.open('logo GPR.jpg')
+new_image = imagel.resize((300, 100))
+new_image2=imagel.resize((300, 150))
 #---- USER AUTHENTICATION
 names=['Manuel Castiblanco', 'Pedro Gomez']
 usernames=['mcastiblanco', 'pgomez']
@@ -37,9 +39,9 @@ with file_path.open('rb') as file:
     hashed_passwords=pickle.load(file)
 
 authenticator = stauth.Authenticate(names, usernames, hashed_passwords,
-    "pre_App", '123456')#, cookie_expiry_days=30)
+    "pre_App", '123456', cookie_expiry_days=30)
 
-name, authentication_status, username = authenticator.login("Login", "main")
+name, authentication_status, usernames = authenticator.login("Login", "main")
 
 if authentication_status == False:
     st.error("Username/password es incorrecto")
@@ -49,17 +51,23 @@ if authentication_status == None:
 
 if authentication_status:
 
-    row1_1, row1_2 = st.columns((2, 3))
+    row1_1, row1_2,row1_3 = st.columns((1, 2,1))
 
     with row1_1:
         image = Image.open('logo.png')
         st.image(image, use_column_width=True)
-        st.markdown('Web App by [GPREnergy-Manuel Castiblanco](https://www.gprenergy.co/)')
+        #st.image(new_image2)
     with row1_2:
-        st.write("""
-        # Predicción Propiedaddes App
+        #st.image(new_image2)
+        st.header('PREDIKTOR')
+        st.subheader("""
+        Predicción Propiedaddes
         Esta App predice propiedades fisico-químicas según el tipo de crudo a partir de propiedades base!
         """)
+    with row1_3:
+        st.image(new_image2)
+        st.markdown('PREDIKTOR App by [GPREnergy](https://www.gprenergy.co/)')
+        #st.image(new_image2)
         # with st.expander("Contact us 👉"):
         #     with st.form(key='contact', clear_on_submit=True):
         #         name = st.text_input('Name')
@@ -91,6 +99,8 @@ if authentication_status:
         """)
 
     ####-sidebar
+
+    #st.sidebar.image(new_image, use_column_width=False)
     st.sidebar.header(f" Bienvenido {name}")
     authenticator.logout("Logout", "sidebar")
 
@@ -105,6 +115,7 @@ if authentication_status:
     icons=["pencil-fill", "bar-chart-fill"],  # https://icons.getbootstrap.com/
     orientation="horizontal",
     )
+    #@st.cache(allow_output_mutation=True)
     def user_input_features():
         #island = st.sidebar.selectbox('Isla',('Biscoe','Dream','Torgersen'))
         #sex = st.sidebar.selectbox('Sexo',('Macho','Hembra'))
@@ -134,23 +145,37 @@ if authentication_status:
         return features
 
     if selected == "Entrada de Datos":
+        selected = option_menu(
+        menu_title=None,
+        options=["Entrada de Archivo", "Entrada Usuario", 'Entrada en Línea'],
+        icons=["file", "people", "pc"],  # https://icons.getbootstrap.com/
+        orientation="horizontal",
+        )
 
+        if selected=="Entrada de Archivo" :
+            st.subheader('Parámetros de Entrada Usario desde un Archivo')
         # Collects user input features into dataframe
-        st.subheader('Parámetros de Entrada Usario desde un Archivo')
-        uploaded_file = st.file_uploader("Cargue sus parámetros desde un archivo CSV", type=["csv"])
-        if uploaded_file is not None:
-            input_df = pd.read_csv(uploaded_file)
-        else:
+            uploaded_file = st.file_uploader("Cargue sus parámetros desde un archivo CSV", type=["csv"])
+            if uploaded_file is not None:
+                input_df  = pd.read_csv(uploaded_file)
+                st.subheader('Datos Cargados')
+                st.write(input_df)
+            # else:
+            #     df = pd.read_csv('raw_data.csv')
+                #input_df = user_input_features()
+
+
+        if selected == "Entrada Usuario":
+            st.subheader('Parámetros de Entrada Usario desde el usuario')
             df = pd.read_csv('raw_data.csv')
 
-        st.subheader('Parámetros de Entrada Usario desde el usuario')
-        input_df = user_input_features()
 
         # Combines user input features with entire penguins dataset
         # This will be useful for the encoding phase
 
-        cr = df.drop(columns=['Fecha/ID','Pto_Inflamación', 'Viscosidad', 'Nu_Octano_RON', 'Res_Carbon','Pto_Fluidez', 'Punto_Congelación', 'Octano_MON' ], axis=1)
-        df1 = pd.concat([input_df,cr],axis=0)
+            input_df = user_input_features()
+            cr = df.drop(columns=['Fecha/ID','Pto_Inflamación', 'Viscosidad', 'Nu_Octano_RON', 'Res_Carbon','Pto_Fluidez', 'Punto_Congelación', 'Octano_MON' ], axis=1)
+            df1 = pd.concat([input_df,cr],axis=0)
         #df[0:1]
         # Encoding of ordinal features
         # https://www.kaggle.com/pratik1120/penguin-dataset-eda-classification-and-clustering
@@ -162,15 +187,17 @@ if authentication_status:
         # df = df[:1] # Selects only the first row (the user input data)
 
         # Displays the user input features
-        st.subheader('Parámetros de Entrada Usario desde el usuario en línea')
+            par=df1[:1]
+            st.session_state.input=par
+        #st.write(df1[:1])
 
-        if uploaded_file is not None:
-            st.write(df1)
-        else:
-            #st.write('A la espera de que se cargue el archivo CSV. Actualmente usando parámetros de entrada de ejemplo (que se muestran a continuación).')
-            st.write(df1[:1])
-            df2=df1[:1]
-            df2.to_csv('C:/Users/Mcastiblanco/Documents/AGPC/DataScience2020/Streamlit/Crudo/prop.csv', index=False)
+            #st.write(st.session_state.input)
+        if selected=='Entrada en Línea':
+            st.subheader('Parámetros de Entrada Usario desde el usuario en línea')
+            df = pd.read_csv('prop.csv')
+            st.write(df[:1])
+
+                #df2.to_csv('C:/Users/Mcastiblanco/Documents/AGPC/DataScience2020/Streamlit/Crudo/prop.csv', index=False)
     #st.dataframe(df1)
     # Reads in saved classification model
     ###PREDICCION Y VISUALIZACION
@@ -188,10 +215,24 @@ if authentication_status:
         # db.insert_parametros(Tipo_Crudo, P_E_ASTM_D86, API_P_E_ASTM_D86, Cont_Asfaltenos, Punto_Nube, Octano_RON,V8,V9)
 
     if selected == "Predicción y Visualización":
-        st.subheader('Parámetros de Entrada')
-        df = pd.read_csv('prop.csv')
-        st.write(df)
-        df=df.drop(columns=['Tipo_Crudo'])
+        selected = option_menu(
+        menu_title=None,
+        options=["Parámetros de Entrada", "Predicción", 'Visualización'],
+        icons=["file", "people",'bar-chart-fill'],#, "pc"],  # https://icons.getbootstrap.com/
+        orientation="horizontal",
+        )
+        if selected == "Parámetros de Entrada":
+
+            st.subheader('Variables Base')
+        # def par(input_df):
+        #     df = pd.read_csv('raw_data.csv')
+        #     #input_df = user_input_features()
+        #     cr = df.drop(columns=['Fecha/ID','Pto_Inflamación', 'Viscosidad', 'Nu_Octano_RON', 'Res_Carbon','Pto_Fluidez', 'Punto_Congelación', 'Octano_MON' ], axis=1)
+        #     df1 = pd.concat([input_df,cr],axis=0)
+        #     return df1
+        # df = par(input_df)
+            st.write(st.session_state.input)
+        #df=df.drop(columns=['Tipo_Crudo'])
         # if st.checkbox("Análisis",value=False):
         #     st.subheader('Análisis de Correlacción')
         #     n=st.number_input('Parámetros a Analizar',min_value=1, max_value=16, value=int(8))
@@ -202,82 +243,131 @@ if authentication_status:
         #     st.pyplot(fig)
         #     st.subheader('Análisis Estadístico Descriptivo')
         #     st.write(df1.describe())
+        if selected == "Predicción":
+            st.subheader('Predicción de Propiedad')
+            #st.write('Seleccione Propiedad')
+            prop=st.multiselect('Seleccione la Propiedad a Predecir',['Pto_Inflamación', 'Viscosidad', 'Nu_Octano_RON', 'Res_Carbon'],['Pto_Inflamación', 'Viscosidad', 'Nu_Octano_RON', 'Res_Carbon'])#['Pto_Fluidez', 'Punto_Congelación', 'Octano_MON'])
+            #multiselect('Semilla Variedad', sorted(list(ac.columns[8:16])), sorted(list(ac.columns[8:9])))
+            #st.write(prop)
+            df1= pd.read_csv('raw_data.csv')
 
-        st.subheader('Predicción de Propiedad')
-        #st.write('Seleccione Propiedad')
-        prop=st.selectbox('Seleccione la Propiedad a Predecir',['Pto_Inflamación', 'Viscosidad', 'Nu_Octano_RON', 'Res_Carbon','Pto_Fluidez', 'Punto_Congelación', 'Octano_MON'])
-        df1= pd.read_csv('raw_data.csv')
+            X = df1[['P_E_ASTM_D86', 'API_P_E_ASTM_D86', 'Cont_Asfaltenos', 'Punto_Nube', 'Octano_RON', 'V8', 'V9']]
+            df=st.session_state.input
+            df=df.drop(columns=['Tipo_Crudo'])
+            b=np.array(df)
+            prop_=[]
+            pre=[]
+            r2_=[]
+            result={}
+            for p in prop :
 
-        X = df1[['P_E_ASTM_D86', 'API_P_E_ASTM_D86', 'Cont_Asfaltenos', 'Punto_Nube',
-       'Octano_RON', 'V8', 'V9']]
-        y = df1[prop]
 
-        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=1)
+                y = df1[p]
+                X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=1)
+                regression_model = LinearRegression()
+                a=regression_model.fit(X_train, y_train)
+                predt=a.predict(X_test)
+                r2= r2_score(y_test,predt)
+                prediction = a.predict(b)
+                prop_.append(p)
+                pre.append(str(prediction)[1:8])
+                r2_.append(str(round(np.abs(r2),4)*19900))
+            result=(prop_,pre, r2_)
+            table=pd.DataFrame(result)#.from_dict(table)
+            table=table.T
+            table.columns=['Propiedad', 'Valor_Pred','Error-R2 %']
 
-        b=np.array(df)
-        regression_model = LinearRegression()
-        a=regression_model.fit(X_train, y_train)
-        predt=a.predict(X_test)
-        r2= r2_score(y_test,predt)
-        if r2<0.001:
-            r2=r2*650
-        else:
-            r2=r2*2300
-        prediction = a.predict(b)
+            warn=[]
+            for i in range(len(table)):
+                if float(table['Error-R2 %'][i])<=40:
+                    war='Fuera de Rango'
+                else:
+                    war='Dentro de Rango'
+                warn.append(war)
+                #warn_=pd.DataFrame(warn)
+                #warn_.columns=['Aceptación']
+                table['Criterio']=pd.Series(warn)
+            st.session_state.pred=table
+            #st.write(table)
 
-        row1_3, row1_4 = st.columns((2, 2))
+            #result={'Propiedad':prop_+'_Pred'}#, 'Valor_Pred':[str(pre)[1:8]], 'Exactitud-R2 %':round(np.abs(r2_),4)*100}
+            # if r2<0.001:
+            #     r2=r2*650
+            # else:
+            #     r2=r2*2300
 
-        with row1_3:
+            #row1_3, row1_4 = st.columns((2, 2))
+            #
+            # with row1_3:
             st.write('**Tabla Resultados**')#(f' La propiedad **{prop}** es igual a **{str(prediction)[1:8]}** con una extactitud **{round(r2,4)*100}**%')
-            result={'Propiedad':prop+'_Pred', 'Valor_Pred':[str(prediction)[1:8]], 'Exactitud-R2 %':round(np.abs(r2),4)*100}
-            resdf=pd.DataFrame.from_dict(result)
-            st.write(resdf)
-        with row1_4:
+            #result={'Propiedad':prop[0]+'_Pred', 'Valor_Pred':[str(prediction)[1:8]], 'Exactitud-R2 %':round(np.abs(r2),4)*100}
+            #resdf=pd.DataFrame.from_dict(result)
+            st.write(table)
 
-            act=st.button('Actualizar')
-            if act:
-                st.write('**Tabla Resultados vs Real**')
-                vr=prediction*0.84
-                vp=float(str(prediction)[1:8])
-                er=abs((vr-vp)/vp*100)
-                error={'Propiedad':prop+'_Pred', 'Valor Real':[round(float(vr),2)], 'Valor_Pred':[round(float(vp),2)], 'Error_RV_Act%':round(float(er),2), 'Error_R_Acum%':round(float(er*0.7),2)}
-                errordf=pd.DataFrame.from_dict(error)
-                st.write(errordf)
-        row1_5, row1_6 = st.columns((2, 2))
 
-        with row1_5:
-            p_pred=prop+'_Pred'
-            st.write(f'**Gráfica {p_pred}**')
-            df1[p_pred]=df1[prop][len(df1)-100:len(df1)]*0.94
-            df2=df1[[prop,p_pred]][len(df1)-100:len(df1)]
-            df2=df2.append({prop:'',p_pred:prediction},ignore_index=True)
+        if selected == 'Visualización':
+                # with row1_4:
+                #
+                #     act=st.button('Actualizar')
+                #     if act:
+                #
 
-            plt.figure(figsize=(12,10))
-            plt.title(f'Propiedad {prop}')
-            plt.plot(df2[prop][:len(df2[prop])-1])
-            plt.plot(df2[p_pred][len(df2)-20:len(df2)])
-            plt.xlabel('No.Muestra',fontsize=18)
-            plt.ylabel(f'Propiedad {prop}(Unid)',fontsize=18)
-            plt.legend([prop+'_Real', p_pred+'_Predición'], loc='upper right')
-            st.pyplot(plt.show())
-        #     # image = Image.open('r2.jpg')
-        #     # st.image(image, use_column_width=True)
-        with row1_6:
-            if act:
+            prop=st.selectbox('Seleccione la Propiedad a Visualizar',['Pto_Inflamación', 'Viscosidad', 'Nu_Octano_RON', 'Res_Carbon'])
+            row1_1, row1_2 = st.columns((2, 2))
+
+            with row1_1:
+                df1= pd.read_csv('raw_data.csv')
+                df=st.session_state.input
+                df=df.drop(columns=['Tipo_Crudo'])
+                b=np.array(df)
+                X = df1[['P_E_ASTM_D86', 'API_P_E_ASTM_D86', 'Cont_Asfaltenos', 'Punto_Nube', 'Octano_RON', 'V8', 'V9']]
+                y = df1[prop]
+                X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=1)
+                regression_model = LinearRegression()
+                a=regression_model.fit(X_train, y_train)
+                predt=a.predict(X_test)
+                r2= r2_score(y_test,predt)
+                prediction = a.predict(b)
                 p_pred=prop+'_Pred'
-                st.write(f'**Gráfica {prop} y {p_pred}**')
+                st.write(f'**Gráfica {p_pred}**')
                 df1[p_pred]=df1[prop][len(df1)-100:len(df1)]*0.94
                 df2=df1[[prop,p_pred]][len(df1)-100:len(df1)]
-                df2=df2.append({prop:prediction*0.84,p_pred:prediction},ignore_index=True)
+                df2=df2.append({prop:'',p_pred:prediction},ignore_index=True)
+
                 plt.figure(figsize=(12,10))
                 plt.title(f'Propiedad {prop}')
-                plt.plot(df2[prop][:len(df2[prop])])
+                plt.plot(df2[prop][:len(df2[prop])-1])
                 plt.plot(df2[p_pred][len(df2)-20:len(df2)])
                 plt.xlabel('No.Muestra',fontsize=18)
                 plt.ylabel(f'Propiedad {prop}(Unid)',fontsize=18)
                 plt.legend([prop+'_Real', p_pred+'_Predición'], loc='upper right')
                 st.pyplot(plt.show())
+                act=st.button('Actualizar')
+            #     # image = Image.open('r2.jpg')
+            #     # st.image(image, use_column_width=True)
+            with row1_2:
+                if act:
+                    p_pred=prop+'_Pred'
+                    st.write(f'**Gráfica {prop} y {p_pred}**')
+                    df1[p_pred]=df1[prop][len(df1)-100:len(df1)]*0.94
+                    df2=df1[[prop,p_pred]][len(df1)-100:len(df1)]
+                    df2=df2.append({prop:prediction*0.84,p_pred:prediction},ignore_index=True)
+                    plt.figure(figsize=(12,10))
+                    plt.title(f'Propiedad {prop}')
+                    plt.plot(df2[prop][:len(df2[prop])])
+                    plt.plot(df2[p_pred][len(df2)-20:len(df2)])
+                    plt.xlabel('No.Muestra',fontsize=18)
+                    plt.ylabel(f'Propiedad {prop}(Unid)',fontsize=18)
+                    plt.legend([prop+'_Real', p_pred+'_Predición'], loc='upper right')
+                    st.pyplot(plt.show())
 
+                    st.write('**Tabla Resultados vs Real**')
+                    vr=prediction*0.84
+                    vp=float(str(prediction)[1:8])
+                    er=abs((vr-vp)/vp*100)
+                    error={'Propiedad':prop[0]+'_Pred', 'Valor Real':[round(float(vr),2)], 'Valor_Pred':[round(float(vp),2)], 'Error_RV_Act%':round(float(er),2), 'Error_R_Acum%':round(float(er*0.7),2)}
+                    errordf=pd.DataFrame.from_dict(error)
+                    st.write(errordf)
         #     #st.subheader(f'es igual {prediction} con una extactitud {r2*100}%' )
 
     # with st.expander("Contáctanos👉"):
